b53dfc46001c4cf970cc6d4bafc06b5f32aa6eeb
vicdev <vtavares.tur@gmail.com>
2022-05-10 15:06:05 -0300

    Implementing Tests @ NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,6 +1,28 @@
-/* import React from 'react'; */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+import App from '../App';
 
-describe('', () => {
-  test('', () => {});
-  test('', () => {});
+describe('Teste o componente <NotFound.js />', () => {
+  test('Se a página contém um heading h2 com o texto Page requested not found ', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notfound');
+
+    const notFoundTitle = screen.getByRole(
+      'heading',
+      { level: 2, name: /Page requested not found Crying emoji/i },
+    );
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    render(<NotFound />);
+    const queryImage = screen.getByRole(
+      'img',
+      { name: /pikachu crying because the page requested was not found/i },
+    );
+    expect(queryImage).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
 });
